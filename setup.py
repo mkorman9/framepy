293b21dda429b6b56bd461f27cb576530223fd03
mkorman9 <m.korman94@gmail.com>
2016-11-06 23:35:53 +0100

    0.29

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open('requirements.txt') as f:
 setup(
     name='framepy',
     packages=find_packages(),
-    version='0.28',
+    version='0.29',
     description='Very simple web application framework with support for AMQP and DI',
     author='Michal Korman',
     author_email='m.korman94@gmail.com',
     url='https://github.com/mkorman9/framepy',
-    download_url='https://github.com/mkorman9/framepy/tarball/0.28',
+    download_url='https://github.com/mkorman9/framepy/tarball/0.29',
     keywords=['web', 'framework', 'amqp', 'di', 'db'],
     classifiers=[],
     install_requires=[requirement for requirement in requirements if len(requirement) > 0]
