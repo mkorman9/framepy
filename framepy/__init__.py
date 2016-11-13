74257952af8cf3e1836d46eaad6cd35d5e37b556
mkorman9 <m.korman94@gmail.com>
2016-11-13 12:55:40 +0100

    rest client

--- framepy/__init__.py
+++ framepy/__init__.py
@@ -3,5 +3,7 @@ import db
 import core
 import testing
 import beans
+import client
 
-__all__ = ['beans', 'core', 'web', 'db', 'testing']
+
+__all__ = ['beans', 'core', 'web', 'db', 'testing', 'client']
