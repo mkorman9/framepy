4df3bb2ed808e82fc4ea72dc155dc32e310d440b
Michal Korman <michal.korman@nokia.com>
2016-11-21 13:18:18 +0100

    build fix

--- framepy/__init__.py
+++ framepy/__init__.py
@@ -4,6 +4,6 @@ import core
 import testing
 import beans
 import client
-from core import log, controller, listener, bean
+from core import log, controller, bean
 
-__all__ = ['beans', 'core', 'web', 'db', 'testing', 'client', 'log', 'controller', 'listener', 'bean']
+__all__ = ['beans', 'core', 'web', 'db', 'testing', 'client', 'log', 'controller', 'bean']
