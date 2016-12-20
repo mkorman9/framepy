8476175a0f284602eee273e3e5133910ae84df5d
Michal Korman <michal.korman@nokia.com>
2016-12-20 16:36:22 +0100

    version 3.61

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.readlines()
 
-version = '3.60'
+version = '3.61'
 
 setup(
     name='framepy',
