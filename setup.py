34c6de1ec76f38a193558c9454efd30f69cc6d68
Michal Korman <michal.korman@nokia.com>
2016-12-21 11:54:21 +0100

    setup.py metadata update

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setup(
     name='framepy',
     packages=find_packages(exclude=('tests',)),
     version=version,
-    description='Very simple web application framework with support for AMQP and DI',
+    description='Simple web application framework inspired by Spring Framework',
     author='Michal Korman',
     author_email='m.korman94@gmail.com',
     url='https://github.com/mkorman9/framepy',
     download_url='https://github.com/mkorman9/framepy/tarball/{}'.format(version),
-    keywords=['web', 'framework', 'amqp', 'di', 'db'],
+    keywords=['web', 'framework', 'amqp', 'di', 'db', 'eureka', 'redis'],
     classifiers=[],
     install_requires=[requirement for requirement in requirements if len(requirement) > 0]
 )
