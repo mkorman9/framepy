c38c41d7766c4686f17d65c31286b559f759658b
mkorman9 <m.korman94@gmail.com>
2016-12-24 13:34:39 +0100

    tests for amqp

--- tests/amqp_test.py
+++ tests/amqp_test.py
@@ -1,8 +1,13 @@
+import threading
 import unittest
 
 from assertpy import assert_that
+from mock import mock
+import pika
+import pika.exceptions
 
 from framepy import amqp
+from framepy import _thread_level_cache
 
 
 class AmqpTest(unittest.TestCase):
@@ -65,3 +70,41 @@ class AmqpTest(unittest.TestCase):
         assert_that(beans['amqp_engine'].port).is_equal_to(amqp.DEFAULT_AMQP_PORT)
         assert_that(beans['amqp_engine'].credentials.username).is_equal_to('test')
         assert_that(beans['amqp_engine'].credentials.password).is_equal_to('test123')
+
+    @mock.patch('pika.BlockingConnection')
+    def test_should_establish_connection_and_return_working_channel(self, blocking_connection):
+        # given
+        self._clear_thread_level_cache()
+
+        context = mock.MagicMock()
+        expected_channel = blocking_connection.return_value.channel.return_value
+
+        # when
+        channel = amqp.get_channel(context)
+
+        # then
+        assert_that(channel).is_equal_to(expected_channel)
+        blocking_connection.assert_called_once_with(context.amqp_engine)
+
+    @mock.patch('pika.BlockingConnection')
+    def test_should_raise_connection_error_if_cannot_connect_to_broker(self, blocking_connection):
+        # given
+        self._clear_thread_level_cache()
+        self._mock_wait_time()
+
+        context = mock.MagicMock()
+        blocking_connection.side_effect = pika.exceptions.ConnectionClosed()
+
+        # when then
+        with self.assertRaises(amqp.ConnectionError):
+            amqp.get_channel(context)
+
+        assert_that(blocking_connection.call_count).is_equal_to(3)
+
+    @staticmethod
+    def _clear_thread_level_cache():
+        _thread_level_cache.cache = threading.local()
+
+    @staticmethod
+    def _mock_wait_time():
+        amqp.WAIT_TIME_AFTER_CONNECTION_FAILURE = 0
