6195dbc43f9b4cc2f45d1edd3bd43184f471f388
Michal Korman <michal.korman@nokia.com>
2016-12-28 10:23:50 +0100

    using _utils in aws module

--- framepy/aws.py
+++ framepy/aws.py
@@ -1,12 +1,29 @@
 from . import modules
+from . import _utils
 
 
 class Module(modules.Module):
     def before_setup(self, properties, arguments, beans):
-        aws_access_key = properties['aws_access_key']
-        aws_access_secret_key = properties['aws_access_secret_key']
-        aws_region = properties['aws_region']
-        endpoint_url = self._filter_missing_endpoint(properties.get('aws_endpoint_url', ''))
+        aws_access_key = _utils.resolve_property_or_report_error(
+            properties=properties,
+            key='aws_access_key',
+            log_message='[AWS] No aws_access_key found in properties'
+        )
+        aws_access_secret_key = _utils.resolve_property_or_report_error(
+            properties=properties,
+            key='aws_access_secret_key',
+            log_message='[AWS] No aws_access_secret_key found in properties'
+        )
+        aws_region = _utils.resolve_property_or_report_error(
+            properties=properties,
+            key='aws_region',
+            log_message='[AWS] No aws_region found in properties'
+        )
+        endpoint_url = _utils.resolve_property_or_report_error(
+            properties=properties,
+            key='aws_endpoint_url',
+            log_message='[AWS] No aws_endpoint_url found in properties'
+        )
 
         beans['aws_credentials'] = {
             'aws_access_key_id': aws_access_key,
