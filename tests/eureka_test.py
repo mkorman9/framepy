fc13f05f40aafea8e9d4e5c1aea2a568479737c2
Michal Korman <michal.korman@nokia.com>
2017-01-02 13:51:24 +0100

    Extracting Eureka functionalities to EurekaTemplate

--- tests/eureka_test.py
+++ tests/eureka_test.py
@@ -9,7 +9,7 @@ from framepy import eureka
 
 class EurekaTest(unittest.TestCase):
     @mock.patch('threading.Thread')
-    @mock.patch('framepy.eureka._get_session_from_cache')
+    @mock.patch('framepy.eureka.EurekaTemplate._get_session_from_cache')
     def test_eureka_should_be_initialized(self, get_session, thread_class):
         # given
         properties = self._prepare_fake_properties()
@@ -30,7 +30,7 @@ class EurekaTest(unittest.TestCase):
         thread.start.assert_called_once_with()
 
     @mock.patch('threading.Thread')
-    @mock.patch('framepy.eureka._get_session_from_cache')
+    @mock.patch('framepy.eureka.EurekaTemplate._get_session_from_cache')
     @mock.patch('framepy.log.error')
     def test_should_log_error_if_cannot_register_instance_due_to_service_error(self, error_log, get_session, thread_class):
         # given
@@ -48,7 +48,7 @@ class EurekaTest(unittest.TestCase):
         error_log.assert_called_once_with('[Eureka] Cannot register instance on server! Status code 404')
 
     @mock.patch('threading.Thread')
-    @mock.patch('framepy.eureka._get_session_from_cache')
+    @mock.patch('framepy.eureka.EurekaTemplate._get_session_from_cache')
     @mock.patch('framepy.log.error')
     def test_should_log_error_if_cannot_register_instance_due_connection_error(self, error_log, get_session, thread_class):
         # given
@@ -65,34 +65,40 @@ class EurekaTest(unittest.TestCase):
         # then
         error_log.assert_called_once_with('[Eureka] Cannot connect to server!')
 
-    @mock.patch('framepy.eureka._get_session_from_cache')
+    @mock.patch('framepy.eureka.EurekaTemplate._get_session_from_cache')
     def test_should_list_instances(self, get_session_from_cache):
         # given
         context = mock.MagicMock()
         service_name = 'some_service'
+
+        eureka_template = eureka.EurekaTemplate()
+        eureka_template.context = context
 
         service_response = b'{"application": {"instance":[{"hostName":"localhost", "port":{"$":8080}}]}}'
         session = get_session_from_cache.return_value
         session.get.return_value = self._fake_ok_response(service_response)
 
         # when
-        instances = eureka.list_instances(context, service_name)
+        instances = eureka_template.list_instances(service_name)
 
         # then
         assert_that(instances).is_equal_to(['localhost:8080'])
 
-    @mock.patch('framepy.eureka._get_session_from_cache')
+    @mock.patch('framepy.eureka.EurekaTemplate._get_session_from_cache')
     def test_should_throw_exeception_if_cannot_find_service(self, get_session_from_cache):
         # given
         context = mock.MagicMock()
         service_name = 'some_service'
+
+        eureka_template = eureka.EurekaTemplate()
+        eureka_template.context = context
 
         session = get_session_from_cache.return_value
         session.get.return_value = self._fake_not_ok_response()
 
         # when then
         with self.assertRaises(Exception):
-            eureka.list_instances(context, service_name)
+            eureka_template.list_instances(service_name)
 
     def _prepare_fake_properties(self):
         return {
